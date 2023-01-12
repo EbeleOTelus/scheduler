420c093ee5ba6343eb18f50a98c4ec160530112d
Ebele <ebele.omanukwue@telus.com>
2023-01-12 14:58:26 -0700

    use axios to fetch data from scheduler-api in application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,9 @@
 import React from "react";
 import DayList from "./DayList";
 import "components/Application.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Appointment from "./Appointment";
+import axios from "axios";
 
 
 const appointments = {
@@ -15,7 +16,7 @@ const appointments = {
     time: "1pm",
     interview: {
       student: "Lydia Miller-Jones",
-      interviewer:{
+      interviewer: {
         id: 3,
         name: "Sylvia Palmer",
         avatar: "https://i.imgur.com/LpaY82x.png",
@@ -31,7 +32,7 @@ const appointments = {
     time: "3pm",
     interview: {
       student: "Archie Andrews",
-      interviewer:{
+      interviewer: {
         id: 4,
         name: "Cohana Roy",
         avatar: "https://i.imgur.com/FK8V841.jpg",
@@ -46,64 +47,52 @@ const appointments = {
 
 export default function Application(props) {
 
-  const [day, setDay] = useState("Monday")
+  const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
 
-  
-
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+  useEffect (() => {
+    axios.get("/api/days").then(response => {
+      console.log(response);
+      setDays(response.data);
+    });
+  }, []);
 
   const appointmentValue = Object.values(appointments).map((appointment) => {
-    return(
+    return (
       <Appointment
-      key={appointment.id} 
-  {...appointment} 
+        key={appointment.id}
+        {...appointment}
 
 
-/>
-    )
+      />
+    );
 
-  })
+  });
 
   return (
     <main className="layout">
       <section className="sidebar">
-      <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-  <DayList 
-  days={days}
-  value={day}
-  onChange={setDay}/>
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList
+            days={days}
+            value={day}
+            onChange={setDay} />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-     {appointmentValue}
-     <Appointment key="last" time="5pm" />
+        {appointmentValue}
+        <Appointment key="last" time="5pm" />
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
       </section>
     </main>

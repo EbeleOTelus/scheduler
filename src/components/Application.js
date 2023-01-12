7aff309af2c29ba2b07f4ac52bfd3b9fe9e792aa
Ebele <ebele.omanukwue@telus.com>
2023-01-12 16:52:19 -0700

    change state to an object

--- src/components/Application.js
+++ src/components/Application.js
@@ -45,10 +45,17 @@ const appointments = {
   }
 };
 
-export default function Application(props) {
 
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+export default function Application(props) {
+  
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+  
+  const setDays = days => setState(prev => ({ ...prev, days }));;
+  const setDay = day => setState(prev => ({ ...prev, day }));
 
   useEffect (() => {
     axios.get("/api/days").then(response => {
@@ -80,8 +87,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay} />
         </nav>
         <img

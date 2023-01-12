207f61698bcd8326526281b22db87e22efb8e86a
Ebele <ebele.omanukwue@telus.com>
2023-01-11 17:41:37 -0700

    create Empty component with its story in index.js

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Empty from "components/Appointment/Empty.js";
+import Header from "components/Appointment/Header.js";
 import Appointment from "components/Appointment/index.js";
 import InterviewerList from "components/InterviewerList";
 import InterviewerListItem from "components/InterviewerListItem";
@@ -139,4 +141,6 @@ storiesOf("Appointment", module)
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
   .add("Appointment", () => <Appointment />)
-  .add("Appointment with Time", () => <Appointment time="12pm" />);
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")}/>)

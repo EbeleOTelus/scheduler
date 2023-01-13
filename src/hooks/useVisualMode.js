23afa64ead4d8722339a54bfc5c5cb67686d18c5
Ebele <ebele.omanukwue@telus.com>
2023-01-13 15:11:02 -0700

    update transition function and implement back function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,12 +1,32 @@
-import { useState } from "react"
+import { useState } from "react";
 
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial)
-  // const newMode = [...mode]
- const transition = (newMode) => {
-  setMode(newMode)
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
- }
-return { mode, transition }
+  const transition = (newMode, replace) => {
+    setMode(newMode);
+    setHistory(prev => ([...prev, newMode]));
+    if (replace) {
+      setHistory(history);
+    }
+  };
+
+  const back = () => {
+    if (history.length > 1) {
+      const newHistory = [...history];
+      newHistory.pop();
+      const lastItemIndex = newHistory.length - 1;
+      setMode(newHistory[lastItemIndex]);
+      setHistory(newHistory);
+    }
+  };
+
+  return { mode, transition, back };
 }
+
+
+// const plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato']
+// const previousItemIndex = history.length-1
+// setMode(history[history.length-1])
